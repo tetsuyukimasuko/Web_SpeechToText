e63847bfe36eb0d451656ec49b8275c28f009bde
tetsuyukimasuko <35556884+tetsuyukimasuko@users.noreply.github.com>
2018-06-08 17:23:58 +0900

    Update settings.py

--- Web_SpeechToText_Service_v1/settings.py
+++ Web_SpeechToText_Service_v1/settings.py
@@ -105,6 +105,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
